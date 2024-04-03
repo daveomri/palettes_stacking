4b04930cec1f1c62a47af89835b65e2645eafb27
David Omrai <david.omrai@seznam.cz>
2024-04-03 14:38:59 +0200

    Fixing the page, adding the dynamic form, not yet done

--- static/index.js
+++ static/index.js
@@ -1,7 +1,6 @@
-var formfield = document.getElementById('formfield');
-
 function add(){
-  var newField = document.createElement('input');
+  let formfield = document.getElementById('formfield');
+  let newField = document.createElement('input');
   newField.setAttribute('type','text');
   newField.setAttribute('name','text');
   newField.setAttribute('class','text');
@@ -11,21 +10,10 @@ function add(){
 }
 
 function remove(){
-  var input_tags = formfield.getElementsByTagName('input');
+  let formfield = document.getElementById('formfield');
+  let input_tags = formfield.getElementsByTagName('input');
   if(input_tags.length > 2) {
     formfield.removeChild(input_tags[(input_tags.length) - 1]);
   }
 }
-
-let form = document.querySelector('#palettes-form');
-form.addEventListener('submit', function (event) {
-	// Ignore the #toggle-something button
-	if (event.submitter.matches('.input')) {
-		event.preventDefault();
-	}
-
-    if (event.submitter.matches('.remove')) {
-		event.preventDefault();
-	}
-});
 
